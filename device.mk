46d0b05645af927a2447b63dedd8ebae9d6a6a78
Han Wang <416810799@qq.com>
2018-10-02 14:00:56 +0200

    onyx: Update textclassifier package name for P
    Change-Id: If846cc590060025202fa336af4420747f6b01238

--- device.mk
+++ device.mk
@@ -194,10 +194,6 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
 
-# TextClassifier smart selection model files
-PRODUCT_PACKAGES += \
-    textclassifier.smartselection.bundle1
-
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.onyx
@@ -259,6 +255,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf
+
+# TextClassifier smart selection model files
+PRODUCT_PACKAGES += \
+    textclassifier.bundle1
 
 # Vibrator
 PRODUCT_PACKAGES += \
