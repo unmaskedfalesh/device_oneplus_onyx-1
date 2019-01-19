adb395e93d4fc4261ab16fa59038e7c4f7d69c79
kushagra57 <kushagra57@yahoo.com>
2019-01-19 19:35:21 +0530

    Revert "onyx: doze: Move Utils.checkDozeService in an handler"
    This reverts commit 60223560f9b1ddea7182a13eaa38b179443685a0.
    
    	modified:   doze/src/org/lineageos/settings/doze/DozeSettingsFragment.java
    	modified:   doze/src/org/lineageos/settings/doze/Utils.java

--- doze/src/org/lineageos/settings/doze/DozeSettingsFragment.java
+++ doze/src/org/lineageos/settings/doze/DozeSettingsFragment.java
@@ -26,7 +26,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v14.preference.PreferenceFragment;
 import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
@@ -50,8 +49,6 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
     private SwitchPreference mPickUpPreference;
     private SwitchPreference mHandwavePreference;
     private SwitchPreference mPocketPreference;
-
-    private Handler mHandler = new Handler();
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -127,9 +124,10 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (Utils.ALWAYS_ON_DISPLAY.equals(preference.getKey())) {
             Utils.enableAlwaysOn(getActivity(), (Boolean) newValue);
+        } else {
+            Utils.enableGesture(getActivity(), preference.getKey(), (Boolean) newValue);
         }
-
-        mHandler.post(() -> Utils.checkDozeService(getActivity()));
+        Utils.checkDozeService(getActivity());
 
         return true;
     }
