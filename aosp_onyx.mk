2de8cfd947e1a72afc0d2acf4ce7f1d396611926
Falesh Chaudhary <unmaskedfalesh@gmail.com>
2019-05-31 13:16:44 +0545

    Update and rename rr_onyx.mk to aosp_onyx.mk

--- aosp_onyx.mk
+++ aosp_onyx.mk
@@ -21,15 +21,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l_mr1.mk
 # Inherit from onyx device
 $(call inherit-product, device/oneplus/onyx/device.mk)
 
-# Inherit some common LineageOS/RR stuff.
-$(call inherit-product, vendor/rr/config/common_full_phone.mk)
-$(call inherit-product, vendor/rr/config/BoardConfigLineage.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/BoardConfigAosp.mk)
 
 
 # Call the proprietary setup
 $(call inherit-product, vendor/oneplus/onyx/onyx-vendor.mk)
 
-PRODUCT_NAME := rr_onyx
+PRODUCT_NAME := aosp_onyx
 PRODUCT_DEVICE := onyx
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_MODEL := OnePlus
